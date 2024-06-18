a4390c7be8b156948b1272a65f0a363d8a39ee59
Ruchik <ruchikjoshi1998@gmail.com>
2024-06-18 22:14:04 +0200

    Adding filter and search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,28 +1,86 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ResCards from './ResCards';
-import resData from '../utils/mokeData';
+import Shimmer from './Shimmer';
+
+// Filter the restaurant data according input type
+function filterData(searchText, restaurants) {
+    const resFilterData = restaurants.filter((restaurant) =>
+        restaurant?.info?.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+    return resFilterData;
+}
 
 export default function Body() {
 
-    const [resList, setResList] = useState(resData);
+    const [resList, setResList] = useState([]);
+    const [resFilteredList, setResFilteredList] = useState([]);
+    const [searchText, setSearchText] = useState('');
+    const [errorMessage, setErrorMessage] = useState("");
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    const fetchData = async () => {
+        try {
+            const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.022505&lng=72.5713621&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+            const jsonResponse = await data.json();
+            const restaurantList = jsonResponse?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+            if (restaurantList) {
+                setResList(restaurantList);
+                setResFilteredList(restaurantList);
+            }
+            console.log("Fetched data!");
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     function filterClick(e) {
         const baseRatings = 4.0;
-        let filteredList = resList.filter((restraurant) => restraurant.info.avgRating > baseRatings);
-        console.log(resList);
+        let filteredList = resList.filter((restaurant) => restaurant.info.avgRating > baseRatings);
         setResList(filteredList);
     }
 
+    const searchRes = (searchText, restaurants) => {
+        if (searchText !== '') {
+            const filteredData = filterData(searchText, restaurants);
+            setResFilteredList(filteredData);
+            setErrorMessage("");
+            if (filteredData?.length === 0) {
+                setErrorMessage("No matches restaurant found");
+            }
+        } else {
+            setResFilteredList(restaurants);
+        }
+    };
+
+    // Shimmer UI : Vid 06 Time  remaining 01:20:20
+    if (resList?.length === 0) {
+        return <Shimmer />;
+    }
+
+
     return (
         <div className="bodyContainer">
+            <div className='searchBar'>
+                <input type='text' className='search' id="searchInput" value={searchText} onChange={(e) => setSearchText(e.target.value)} />
+                <button className='searchButton' onClick={() => {
+                    // user click on button searchData function is called
+                    searchRes(searchText, resList);
+                }}>Search</button>
+            </div>
             <div className="filter">
                 <button className='filter-btn' onClick={filterClick} >Top rated restaurants</button>
             </div>
+
+            {errorMessage && <div className="error-container">{errorMessage}</div>}
+
             <div className="resContainer">
                 <div className="resCards">
                     {
-                        resList.map((restaurant) => {
+                        resFilteredList.map((restaurant) => {
                             return (<ResCards key={restaurant.info.id} resData={restaurant} />);
                         }
                         )
---
+++ src/components/Shimmer.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Shimmer() {
+    return (
+        <div className="bodyContainer">
+            <div className='shimmer-container'>
+                <div className='shimmer-buttons'>
+                    <div className='shimmer-dummy-button'></div>
+                    <div className='shimmer-dummy-button'></div>
+                </div>
+                <div className="resContainer">
+                    <div className="resCards">
+                        <div className="card">
+
+                        </div>
+                        <div className="card">
+
+                        </div>
+                        <div className="card">
+
+                        </div>
+                        <div className="card">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
